eac5bd0571f79e455922227943ce04483ee1621c
Maksym-Gorbunov <maxsverige@hotmail.com>
2020-06-04 13:00:05 +0200

    cors bean added to config

--- src/main/java/com/mg/covid19/config/Config.java
+++ src/main/java/com/mg/covid19/config/Config.java
@@ -6,6 +6,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class Config {
@@ -29,6 +32,16 @@ public class Config {
     @Bean
     public RestHelper responseTransformer() {
         return new RestHelper();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 
 }
